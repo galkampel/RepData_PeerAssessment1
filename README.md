51860d6d003fb4c727d39a0bdaba9994b13a9f60
Gal Kampel <galkampel@gmail.com>
2015-11-14 13:45:33 +0200

    submission off peer assessment 1

--- README.md
+++ README.md
@@ -1,23 +1,3 @@
-## Introduction
-
-It is now possible to collect a large amount of data about personal
-movement using activity monitoring devices such as a
-[Fitbit](http://www.fitbit.com), [Nike
-Fuelband](http://www.nike.com/us/en_us/c/nikeplus-fuelband), or
-[Jawbone Up](https://jawbone.com/up). These type of devices are part of
-the "quantified self" movement -- a group of enthusiasts who take
-measurements about themselves regularly to improve their health, to
-find patterns in their behavior, or because they are tech geeks. But
-these data remain under-utilized both because the raw data are hard to
-obtain and there is a lack of statistical methods and software for
-processing and interpreting the data.
-
-This assignment makes use of data from a personal activity monitoring
-device. This device collects data at 5 minute intervals through out the
-day. The data consists of two months of data from an anonymous
-individual collected during the months of October and November, 2012
-and include the number of steps taken in 5 minute intervals each day.
-
 ## Data
 
 The data for this assignment can be downloaded from the course web
@@ -79,6 +59,11 @@ assignment so you do not have to download the data separately.
 Show any code that is needed to
 
 1. Load the data (i.e. `read.csv()`)
+
+```{r}
+activity <- read.csv("reproducible_research/RepData_PeerAssessment1/activity/activity.csv"")
+
+```
 
 2. Process/transform the data (if necessary) into a format suitable for your analysis
 
@@ -166,3 +151,6 @@ https://github.com/rdpeng/RepData_PeerAssessment1
 
 7c376cc5447f11537f8740af8e07d6facc3d9645
 ```
+
+
+
--- tmp.R
+++ tmp.R
@@ -1,67 +1,57 @@
----
-title: "Peer_Assessment_1"
-author: "Gal"
-output: html_document
----
 
-
-
-##Calculating the total number of steps taken per day
-###Removing missing values from data
-```{r}
+#changing time locale for R (windows)
 Sys.setlocale("LC_TIME", "English")
-activity <- read.csv("activity/activity.csv")
+
+library(ggplot2)
+
+activity <- read.csv("reproducible_research/RepData_PeerAssessment1/activity/activity.csv")
 activity2 <- activity[!is.na(activity$steps),]
 
+
+
+#Calculate the total number of steps taken per day
 total <- tapply(activity2$steps, factor(activity2$date), sum)
-hist(total,xlim = c(0,25000),ylim = c(0,30),main="Total number of steps taken per day",xlab = "steps")
-#saving into a file
-jpeg("figure/hist1.png",width=480,height=480)
+#saving to file
+jpeg("reproducible_research/RepData_PeerAssessment1/figure/hist1.png",width=480,height=480)
 hist(total,xlim = c(0,25000),ylim = c(0,30),main="Total number of steps taken per day",xlab = "steps")
 
 dev.off()
 
-```
 
-###the mean/total of the number of the  number of steps taken per day
-```{r}
+#the mean/total of the number of the  number of steps taken per day
 mean <- tapply(activity2$steps, factor(activity2$date), mean)
 median <- tapply(activity2$steps, factor(activity2$date), median)
 
 mean
 median
 
+#median/total of the total number of steps taken per day
 median(total)
 mean(total)
 
-plot(tapply(activity2$steps, factor(activity2$interval), mean),type = 'l',xlab = "5-minute interval (normalized)", ylab = "average number of steps taken",main=)
-```
 
-###saving into a file
-```{r}
-jpeg("figure/plot.png",width=480,height=480)
+
+#saving to file
+jpeg("reproducible_research/RepData_PeerAssessment1/figure/plot.png",width=480,height=480)
 
 plot(tapply(activity2$steps, factor(activity2$interval), mean),type = 'l',xlab = "5-minute interval (normalized)", ylab = "average number of steps taken",main=)
 
 dev.off()
-```
 
-###the real max interval and the normalized one
-```{r}
+#the real max interval and the normalized one
 which.max(tapply(activity2$steps, factor(activity2$interval), mean))
-```
 
-##Imputing missing values
-```{r}
+
+#Imputing missing values
+
 missing_values <- activity[is.na(activity$steps),]
-
 # the total number of missing values in the dataset
-num_missing_values <- length(missing_values[,1])
-length(median[missing_values[,2]])
-```
 
-##missing value <- the median(total steps of that day)
-```{r}
+num_missing_values <- length(missing_values[,1])
+#missing value <- the median(total steps of that day)
+
+length(median[missing_values[,2]])
+
 missing_values[,1] <- median[missing_values[,2]]
 #if the value of an entire month is na we put 0 instead
 missing_values[is.na(missing_values$steps),1] <- 0
@@ -71,10 +61,9 @@ new_data <- rbind(missing_values,activity2)
 
 new_total <- tapply(new_data$steps, factor(new_data$date), sum)
 
-hist(new_total,xlim = c(0,25000),ylim = c(0,30),main="Total number of steps taken per day",xlab = "steps")
 
 #saving hist to file
-jpeg("figure/hist2.png",width=480,height=480)
+jpeg("reproducible_research/RepData_PeerAssessment1/figure/hist2.png",width=480,height=480)
 
 hist(new_total,xlim = c(0,25000),ylim = c(0,30),main="Total number of steps taken per day",xlab = "steps")
 
@@ -91,21 +80,17 @@ median_1
 #median/total of the total number of steps taken per day
 median(new_total)
 mean(new_total)
-```
 
-##differences in activity patterns between weekdays and weekends
-```{r}
-library(ggplot2)
+
+
+#differences in activity patterns between weekdays and weekends
 new_data[,2] <- factor(weekdays(as.Date(new_data$date)) %in% c('Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'),levels=c(FALSE,TRUE),labels = c('Weekend','Weekday'))
 weekdays_1 <- c('Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday')
 
-
-qplot(interval,steps,data=new_data,stat="summary",fun.y="mean",geom = 'line',facets =date ~ .)
-
 #saving to file
-jpeg("figure/weekday-weekend.png",width=480,height=480)
+jpeg("reproducible_research/RepData_PeerAssessment1/figure/weekday-weekend.png",width=480,height=480)
 
 qplot(interval,steps,data=new_data,stat="summary",fun.y="mean",geom = 'line',facets =date ~ .)
 
 dev.off()
-```
+rm(list=ls())
